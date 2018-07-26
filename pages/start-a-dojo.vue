35750bcdf9ac9cdf3b12ca089872ce0f8c61f211
Maimuna Ahmed <maimuna.p.ahmed@gmail.com>
2018-07-26 12:10:24 +0100

    move start-a-dojo folder to components and change file names

--- pages/start-a-dojo.vue
+++ pages/start-a-dojo.vue
@@ -1,20 +1,20 @@
 <template>
   <div>
-    <StartADojoHero />
-    <StartADojoChampion />
-    <StartADojoQuestions />
+    <Hero />
+    <Champion />
+    <Questions />
   </div>
 </template>
 
 <script>
-import StartADojoHero from '~/components/index/startadojo/StartADojoHero';
-import StartADojoChampion from '~/components/index/startadojo/StartADojoChampion';
-import StartADojoQuestions from '~/components/index/startadojo/StartADojoQuestions';
+import Hero from '~/components/startadojo/Hero';
+import Champion from '~/components/startadojo/Champion';
+import Questions from '~/components/startadojo/Questions';
 export default {
   components: {
-    StartADojoHero,
-    StartADojoChampion,
-    StartADojoQuestions,
+    Hero,
+    Champion,
+    Questions,
   },
 };
 </script>
