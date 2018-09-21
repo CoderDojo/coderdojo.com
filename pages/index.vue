bc7a3574a908eac4f0a43d523d9aba14b5a1d4f3
Wardormeur <wardormeur@gmail.com>
2018-09-21 11:50:04 +0100

    Get translations in OG tags clean up non-existing meta-tags

--- pages/index.vue
+++ pages/index.vue
@@ -23,18 +23,19 @@ import Facebook from '~/components/index/Facebook';
 export default {
   head () {
     return {
-      title: 'CoderDojo – Enabling young people worldwide to create and explore technology together',
+      title: this.$t('CoderDojo – Enabling young people worldwide to create and explore technology together'),
       meta: [
-        { hid: 'og:title', name: 'og:title', property: 'og:title', content: 'CoderDojo – Enabling young people worldwide to create and explore technology together' },
+        { hid: 'description', name: 'description', property: 'description', content: this.$t('CoderDojo is a global movement of free, volunteer-led, community-based computer programming clubs for young people. Children can visit a Dojo where they can learn to code, build websites, create apps or games, and explore technology in an creative and social environment.') },
+        { hid: 'og:title', name: 'og:title', property: 'og:title', content: this.$t('CoderDojo – Enabling young people worldwide to create and explore technology together') },
         { hid: 'og:site-name', name: 'og:site-name', property: 'og:site-name', content: 'CoderDojo' },
         { hid: 'og:url', name: 'og:url', property: 'og:url', content: 'https://coderdojo.com' },
         { hid: 'og:type', name: 'og:type', property: 'og:type', content: 'website' },
         { hid: 'og:image', name: 'og:image', property: 'og:image', content: '~/static/images/index/meta-header.png' },
-        { hid: 'og:description', name: 'og:description', property: 'og:description', content: 'CoderDojo is a global movement of free, volunteer-led, community-based computer programming clubs for young people. Children can visit a Dojo where they can learn to code, build websites, create apps or games, and explore technology in an creative and social environment.' },
-        { hid: 'twitter:title', name: 'twitter:title', property: 'twitter:title', content: 'CoderDojo – Enabling young people worldwide to create and explore technology together' },
+        { hid: 'og:description', name: 'og:description', property: 'og:description', content: this.$t('CoderDojo is a global movement of free, volunteer-led, community-based computer programming clubs for young people. Children can visit a Dojo where they can learn to code, build websites, create apps or games, and explore technology in an creative and social environment.') },
+        { hid: 'twitter:title', name: 'twitter:title', property: 'twitter:title', content: this.$t('CoderDojo – Enabling young people worldwide to create and explore technology together') },
         { hid: 'twitter:url', name: 'twitter:url', property: 'twitter:url', content: 'https://coderdojo.com' },
         { hid: 'twitter:image', name: 'twitter:image', property: 'twitter:image', content: '~/static/images/index/meta-header.png' },
-        { hid: 'twitter:description', name: 'twitter:description', property: 'twitter:description', content: 'CoderDojo is a global movement of free, volunteer-led, community-based computer programming clubs for young people. Children can visit a Dojo where they can learn to code, build websites, create apps or games, and explore technology in an creative and social environment.' },
+        { hid: 'twitter:description', name: 'twitter:description', property: 'twitter:description', content: this.$t('CoderDojo is a global movement of free, volunteer-led, community-based computer programming clubs for young people. Children can visit a Dojo where they can learn to code, build websites, create apps or games, and explore technology in an creative and social environment.') },
         { hid: 'twitter:card', name: 'twitter:card', property: 'twitter:card', content: 'summary_large_image' }
       ]
     }
--- pages/resources.vue
+++ pages/resources.vue
@@ -14,20 +14,19 @@
   export default {
     head () {
       return {
-        title: 'Our range of learning and training resources',
+        title: this.$t('Our range of learning and training resources'),
         meta: [
-          { hid: 'title', name: 'title', property: 'title', content: 'Our range of learning and training resources' },
-          { hid: 'description', name: 'description', property: 'description', content: 'Our collection of learning and training resources will help you in setting up and running your Dojo. The collection includes educational material, exercises and project ideas to give to the Ninjas at your Dojo as well as links to training materials and content for organisers.' },  
-          { hid: 'og:title', name: 'og:title', property: 'og:title', content: 'Our range of learning and training resources' },
+          { hid: 'description', name: 'description', property: 'description', content: this.$t('Our collection of learning and training resources will help you in setting up and running your Dojo. The collection includes educational material, exercises and project ideas to give to the Ninjas at your Dojo as well as links to training materials and content for organisers.') },  
+          { hid: 'og:title', name: 'og:title', property: 'og:title', content: this.$t('Our range of learning and training resources') },
           { hid: 'og:site-name', name: 'og:site-name', property: 'og:site-name', content: 'CoderDojo' },
           { hid: 'og:url', name: 'og:url', property: 'og:url', content: 'https://coderdojo.com/resources/' },
           { hid: 'og:type', name: 'og:type', property: 'og:type', content: 'website' },
           { hid: 'og:image', name: 'og:image', property: 'og:image', content: '~/static/images/resources/meta-header.png' },
-          { hid: 'og:description', name: 'og:description', property: 'og:description', content: 'Our collection of learning and training resources will help you in setting up and running your Dojo. The collection includes educational material, exercises and project ideas to give to the Ninjas at your Dojo as well as links to training materials and content for organisers.' },
-          { hid: 'twitter:title', name: 'twitter:title', property: 'twitter:title', content: 'Our range of learning and training resources' },
+          { hid: 'og:description', name: 'og:description', property: 'og:description', content: this.$t('Our collection of learning and training resources will help you in setting up and running your Dojo. The collection includes educational material, exercises and project ideas to give to the Ninjas at your Dojo as well as links to training materials and content for organisers.') },
+          { hid: 'twitter:title', name: 'twitter:title', property: 'twitter:title', content: this.$t('Our range of learning and training resources') },
           { hid: 'twitter:url', name: 'twitter:url', property: 'twitter:url', content: 'https://coderdojo.com/resources/' },
           { hid: 'twitter:image', name: 'twitter:image', property: 'twitter:image', content: '~/static/images/resources/meta-header.png' },
-          { hid: 'twitter:description', name: 'twitter:description', property: 'twitter:description', content: 'Our collection of learning and training resources will help you in setting up and running your Dojo. The collection includes educational material, exercises and project ideas to give to the Ninjas at your Dojo as well as links to training materials and content for organisers.' },
+          { hid: 'twitter:description', name: 'twitter:description', property: 'twitter:description', content: this.$t('Our collection of learning and training resources will help you in setting up and running your Dojo. The collection includes educational material, exercises and project ideas to give to the Ninjas at your Dojo as well as links to training materials and content for organisers.') },
           { hid: 'twitter:card', name: 'twitter:card', property: 'twitter:card', content: 'summary_large_image' }
         ]
       }
