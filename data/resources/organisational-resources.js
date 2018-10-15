3bb277756dfe733db4c5e278066b1df1e4a5ea6b
Daniel Brierton <daniel@brierton.me>
2018-10-15 17:48:54 +0100

    Fix link to start a dojo course

--- data/resources/organisational-resources.js
+++ data/resources/organisational-resources.js
@@ -4,6 +4,6 @@ export default [
     title: 'Learn how to set up a programming club for young people',
     description: 'If you need to build your confidence and knowledge in order to to start a Dojo just use our online course where you\'ll learn how to help young people explore technology in an informal, creative and social environment.',
     anchorText: 'Our Start a Dojo course',
-    anchorHref: '#',
+    anchorHref: 'https://www.futurelearn.com/courses/start-a-coder-dojo',
   },
 ];
