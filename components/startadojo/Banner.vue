51cd678c6f822b81b44d7f274b4cd182e69627e9
Ant Barnes <ant@arayb.com>
2020-04-22 11:38:20 +0100

    Add covid banner to start a dojo page (#57)

---
+++ components/startadojo/Banner.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="cd-banner__container">
+      <div class="cd-banner">
+        <p>{{ $t('Thank you for your interest in joining the CoderDojo community!') }}</p>
+        <p>{{ $t('Due to the coronavirus pandemic, we have paused the Dojo verification process.') }}</p>
+        <p>{{ $t('In the meantime, we’re providing exciting opportunities for young people, parents, volunteers, and educators to get creative with tech through') }}
+          <a class="cd-banner-link" href="https://www.raspberrypi.org/at-home/">{{ $t('Digital Making at Home') }}</a> {{ $t('from the Raspberry Pi Foundation.') }}
+        </p>
+      </div>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+  @import '~/assets/_variables.scss';
+  .cd-banner__container {
+    background-color: #4C1B62;
+    padding: 32px 0;
+  }
+  .cd-banner {
+    max-width: $content-width;
+    margin: 0 auto;
+    color: #fff;
+  }
+
+  .cd-banner-link {
+    color: #fff;
+    text-decoration: underline;
+    font-weight: bold;
+  }
+</style>
