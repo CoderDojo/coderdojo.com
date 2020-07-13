0f1b00cc85cfceb290b5a2b9e77d12f7d4d37c63
Glen Pike <glen.pike@raspberrypi.org>
2020-07-13 17:59:08 +0100

    Add Arabic to translations (#63)

--- update-trads.sh
+++ update-trads.sh
@@ -3,8 +3,8 @@ git config credential.helper
 git config --global user.email "zenbot+github@coderdojo.org"
 git config --global user.name "ZenPlatformBot"
 git clone https://${GITHUB_AUTH_TOKEN}@github.com/CoderDojo/coderdojo.com.git
-for lang in fr de zh-CN nl el hi hu it ja ko pl ro es-ES tr; do 
-  crowdin download -l ${lang} --verbose; 
+for lang in fr de zh-CN nl el hi hu it ja ko pl ro es-ES tr ar; do
+  crowdin download -l ${lang} --verbose;
 done
 cd coderdojo.com
 git add locales/
