c7bc9e8ede4b1b60e09f0b58fd00d8a9f58c7353
Patrick J Cherry <patch@dominoid.net>
2020-08-21 17:33:28 +0100

    Add restart pages
    First pass at adding the restart pages.  Still need to add in content.

---
+++ components/restart/Champion.vue
@@ -0,0 +1,118 @@
+<template>
+  <div class="c-champion" id="champion">
+    <h2 class="c-champion__header center-text">{{ $t('What is a Champion?') }}</h2>
+    <div class="c-champion__content">
+      <div class="c-champion__content-individual">
+        <div class="c-champion__text">
+          <p>{{ $t('Dojos are championed by individuals all around the world who are passionate about giving young people the opportunity to learn to code. CoderDojo champions are enthusiastic, driven (and often not techie!) volunteers who take charge of setting up and maintaining a Dojo.') }}</p>
+          <p>{{ $t('But champions are never alone! They find volunteers to help them organise their Dojo and mentor the young people. And champions always have CoderDojo\'s resources and our 200+ projects to help them run their club and engage the kids, and they can always ask us, the CoderDojo team, for our help directly.') }}</p>
+        </div>   
+      </div>
+      <div class="c-champion__content-individual">
+        <div class="c-champion__quote-cards">
+          <div class="c-champion__quote-card">
+            <p class="c-champion__quote">“{{ $t('Realising that I had created such a wonderful place for the children also ignited a spark in me.') }}”</p>
+            <div class="c-champion__quote-attr">
+              <img class="c-champion__quote-attr-image" src="~/static/images/startadojo/marloes.jpeg" />
+              <span class="c-champion__quote-attr-text">Marloes, Netherlands</span>
+            </div>
+          </div>
+        </div> 
+      </div>     
+    </div>
+    <div class="c-champion__video">
+      <iframe src="https://www.youtube.com/embed/y5v0dZGZoJY" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+    </div>
+  </div>      
+</template>
+
+<style lang="scss" scoped>
+  @import '~/assets/_variables.scss';
+  .c-champion {
+    max-width: $content-width;
+    margin: 0 auto 40px;
+    padding: 40px 0 40px;
+    &__content {
+      display: flex;
+      max-width: $content-width;
+      margin: 60px auto 0 auto;
+    }
+    &__content-individual {
+      flex: 1;
+      justify-content: space-between;
+    }
+    &__text {
+      max-width: 80%;
+      margin: 0 auto;
+      p {
+        margin-bottom: 40px;
+      }
+    }    
+    &__illustration {
+      display: block;
+      margin: 40px auto 0 auto;
+      width: calc(100% - 80px);
+    }
+    &__quote-cards {
+      display: flex;
+    }
+    &__quote-card {
+      flex: 1;
+      margin: 90px 16px;
+      box-shadow: #F1E7F6 12px 9px 40px 0;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      padding: 24px 32px;
+      border-radius: 5px;
+    }
+    &__quote {
+      &-attr {
+        display: flex;
+        margin-top: 24px;
+        align-items: center;
+        &-image {
+          width: 36px;
+          border-radius: 100%;
+        }
+        &-text {
+          font-size: $subtext;
+          margin-left: 10px;
+        }
+      }
+    }   
+    &__cta {
+      text-align: center;
+      margin: 80px auto 80px auto;
+    }
+
+    &__video {
+      padding: 24px 32px;
+      display: flex;
+      max-width: $content-width;
+      iframe {
+        width: 100%;
+        min-height: 460px;
+      }
+    }     
+  }
+  @include media-breakpoint-max(sm) {
+    .c-champion {
+      &__header {
+        font-size: $mobile-h2-size;
+      }
+      &__quote-cards {
+        flex-direction: column;
+      }
+      &__content {
+        display: block;
+      }
+      &__video {
+        iframe {
+          width: 100%;
+          height: 100%; 
+        }   
+      }        
+    }
+  }
+</style>
---
+++ components/restart/Hero.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="c-hero center-text">
+    <h1 class="c-hero__header1">{{ $t('Options for running your club') }}</h1>
+    <div class="c-hero__intro">
+      <p>CodeDojo clubs have been hugely impacted by the coronavirus pandemic. In early 2020 Volunteers thoughtfully decided when to pause hosting their regular in-person sessions. Since then it’s been inspiring to hear how champions and mentors have adapted their clubs.</p>
+
+      <p>Below are three options to help you continue providing fun and creative, learning opportunities for young people and volunteers. You know your Dojo best - feel free to adapt and combine elements of these options to suit your needs and those of your Dojo members.</p>
+    </div>
+    <div class="c-hero__images">
+      <div class="c-hero__image">
+        <VideoModal img="volunteer" src="https://www.youtube.com/embed/Q6M0IdEn07E" text="Being a mentor"/>
+        <p class="c-hero__image-desc">Online sessions</p>
+      </div>
+      <div class="c-hero__image">
+        <VideoModal img="volunteer" src="https://www.youtube.com/embed/Q6M0IdEn07E" text="Being a mentor"/>
+        <p class="c-hero__image-desc">Remote activities</p>
+      </div>
+      <div class="c-hero__image">
+        <VideoModal img="volunteer" src="https://www.youtube.com/embed/Q6M0IdEn07E" text="Being a mentor"/>
+        <p class="c-hero__image-desc">In-person sessions</p>
+      </div>
+    </div>
+    <div class="c-hero__outro">
+      <p>Not sure which option suits your local situation? <a href=".">Check out our useful framework</a>.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import VideoModal from '../VideoModal';
+export default {
+  components: {
+    VideoModal,
+  },
+};
+</script>
+<style lang="scss" scoped>
+  @import '~/assets/_variables.scss';
+
+  .c-hero {
+    padding: 0 0 80px 0;
+    background-color: $cd-purple;
+
+    &__header1 {
+      color: $white;
+      max-width: 900px;
+      margin: 0 auto;
+      padding: 50px 20px;
+      font-weight: bold;
+    }
+
+    &__images {
+      display: flex;
+      max-width: $content-width;
+      margin: 30px auto 0 auto;
+    }
+
+    &__intro, &__outro {
+      max-width: $content-width;
+      margin: 0 auto;
+      color: $white;
+      text-align: left; 
+
+      p:not(:last-child) {
+        padding-bottom: 1em;
+      }
+    }
+    
+    &__outro {
+      max-width: $content-width;
+      margin: 2em auto 0 auto;
+      color: $white;
+      text-align: center;
+
+      a {
+        color: $white;
+        text-decoration: underline;
+        font-weight: bold;
+      } 
+    }
+
+    &__image {
+      flex: 1;
+      justify-content: space-between;
+      margin: 0 20px;
+
+      img {
+        width: 100%;
+      }
+      &-desc {
+        display: block;
+        font-size: 15px;
+        text-align: center;
+        color: $white;
+      }
+    }
+
+  }
+
+  @include media-breakpoint-max(sm) {
+    .c-hero {
+      background-color: $cd-purple;
+
+      &__header1 {
+        font-size: $mobile-h1-size;
+        font-weight: $mobile-h1-weight;
+      }
+
+      &__images {
+        flex-direction: column;
+      }
+
+      &__image {
+        margin: 20px 40px;
+
+        &:first-child {
+          display: none;
+        }
+      }
+    }
+  }
+</style>
