bfacaa9e50228fb6ccef4ac4a080dcc3f09572a0
Patrick J Cherry <patch@dominoid.net>
2020-08-24 10:41:23 +0100

    Fixed copy; added CTA

--- components/restart/Sessions.vue
+++ components/restart/Sessions.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="c-sessions" id="sessions">
     <h2 id="online-sessions" class="c-sessions__header center-text">{{ $t('Online sessions') }}</h2>
+    <p>You can run online sessions using video conferencing or live-streaming tools. In this option, young people work through a project together as a group, or work independently on different activities. Volunteers support them to write and debug their code in real time.</p>
     <div class="c-sessions__content">
       <div class="c-sessions__content-individual">
         <div class="c-sessions__text">
-	  <p>You can run online sessions using video conferencing or live-streaming tools. In this option, young people work through a project together as a group, or work independently on different activities. Volunteers support them to write and debug their code in real time.</p>
 
           <p>The key considerations of running online sessions are: </p>
           <ul>
@@ -13,7 +13,7 @@
             <li>communicating with parents</li>
             <li>structuring your session</li>
           </ul>
-        </div>   
+        </div>
       </div>
       <div class="c-sessions__content-individual">
         <div class="c-sessions__quote-cards">
@@ -24,10 +24,10 @@
               <span class="c-sessions__quote-attr-text">Marloes, Netherlands</span>
             </div>
           </div>
-        </div> 
-      </div>     
+        </div>
+      </div>
     </div>
-      
+
     <div class="c-sessions__content">
       <div class="c-sessions__content-individual">
         <div class="c-sessions__quote-cards">
@@ -38,37 +38,38 @@
               <span class="c-sessions__quote-attr-text">Marloes, Netherlands</span>
             </div>
           </div>
-        </div> 
-      </div>     
+        </div>
+      </div>
       <div class="c-sessions__content-individual">
         <div class="c-sessions__text">
-	  <h3>Resources</h3>
-	  <ul>
+          <h3>Resources</h3>
+          <ul>
             <li><a href="">Which tool is right for you?</a></li>
             <li><a>Session structure template pdf</a></li>
             <li><a>Code of conduct for children</a></li>
           </ul>
-        </div>   
+        </div>
       </div>
-    </div>     
-    <div class="c-sessions__cta">
-      <p><a href="">Read our guidance</a></p>
     </div>
-    
+    <div class="c-sessions__cta">
+      <p><a class="c-button c-button--orange" href="">Read our guidance</a></p>
+    </div>
+
+    <hr />
+
     <h2 id="remote-activities" class="c-sessions__header center-text">{{ $t('Remote activites') }}</h2>
+    <p>Remote activities include setting projects and challenges for the young people to complete at home. This is particularly suitable if your club participants' access to high-speed internet is limited.</p>
     <div class="c-sessions__content">
       <div class="c-sessions__content-individual">
         <div class="c-sessions__text">
-	  <p>You can run online sessions using video conferencing or live-streaming tools. In this option, young people work through a project together as a group, or work independently on different activities. Volunteers support them to write and debug their code in real time.</p>
-
-          <p>The key considerations of running online sessions are: </p>
+          <p>The key aspects to sending remote activities are:</p>
           <ul>
             <li>ensuring safety of children online</li>
-            <li>deciding on your tool </li>
+            <li>deciding on your tool</li>
             <li>communicating with parents</li>
             <li>structuring your session</li>
-          </ul>
-        </div>   
+           </ul>
+        </div>
       </div>
       <div class="c-sessions__content-individual">
         <div class="c-sessions__quote-cards">
@@ -79,65 +80,10 @@
               <span class="c-sessions__quote-attr-text">Marloes, Netherlands</span>
             </div>
           </div>
-        </div> 
-      </div>     
-    </div>
-      
-    <div class="c-sessions__content">
-      <div class="c-sessions__content-individual">
-        <div class="c-sessions__quote-cards">
-          <div class="c-sessions__quote-card">
-            <p class="c-sessions__quote">“{{ $t('Realising that I had created such a wonderful place for the children also ignited a spark in me.') }}”</p>
-            <div class="c-sessions__quote-attr">
-              <img class="c-sessions__quote-attr-image" src="~/static/images/startadojo/marloes.jpeg" />
-              <span class="c-sessions__quote-attr-text">Marloes, Netherlands</span>
-            </div>
-          </div>
-        </div> 
-      </div>     
-      <div class="c-sessions__content-individual">
-        <div class="c-sessions__text">
-	  <h3>Resources</h3>
-	  <ul>
-            <li><a href="">Which tool is right for you?</a></li>
-            <li><a>Session structure template pdf</a></li>
-            <li><a>Code of conduct for children</a></li>
-          </ul>
-        </div>   
+        </div>
       </div>
-    </div>     
-    <div class="c-sessions__cta">
-      <p><a href="">Read our guidance</a></p>
     </div>
-    
-   <h2 id="in-person-sessions" class="c-sessions__header center-text">{{ $t('In-person sessions') }}</h2>
-    <div class="c-sessions__content">
-      <div class="c-sessions__content-individual">
-        <div class="c-sessions__text">
-	  <p>You can run online sessions using video conferencing or live-streaming tools. In this option, young people work through a project together as a group, or work independently on different activities. Volunteers support them to write and debug their code in real time.</p>
 
-          <p>The key considerations of running online sessions are: </p>
-          <ul>
-            <li>ensuring safety of children online</li>
-            <li>deciding on your tool </li>
-            <li>communicating with parents</li>
-            <li>structuring your session</li>
-          </ul>
-        </div>   
-      </div>
-      <div class="c-sessions__content-individual">
-        <div class="c-sessions__quote-cards">
-          <div class="c-sessions__quote-card">
-            <p class="c-sessions__quote">“{{ $t('Realising that I had created such a wonderful place for the children also ignited a spark in me.') }}”</p>
-            <div class="c-sessions__quote-attr">
-              <img class="c-sessions__quote-attr-image" src="~/static/images/startadojo/marloes.jpeg" />
-              <span class="c-sessions__quote-attr-text">Marloes, Netherlands</span>
-            </div>
-          </div>
-        </div> 
-      </div>     
-    </div>
-      
     <div class="c-sessions__content">
       <div class="c-sessions__content-individual">
         <div class="c-sessions__quote-cards">
@@ -148,23 +94,85 @@
               <span class="c-sessions__quote-attr-text">Marloes, Netherlands</span>
             </div>
           </div>
-        </div> 
-      </div>     
+        </div>
+      </div>
       <div class="c-sessions__content-individual">
         <div class="c-sessions__text">
-	  <h3>Resources</h3>
-	  <ul>
-            <li><a href="">Which tool is right for you?</a></li>
-            <li><a>Session structure template pdf</a></li>
-            <li><a>Code of conduct for children</a></li>
+          <h3>Resources</h3>
+          <ul>
+            <li><a href="">Remote activities planning checklist</a></li>
+            <li><a href="">Tips for engaging with parents and ninjas</a></li>
+            <li><a href="">Learning pathway</a></li>
           </ul>
-        </div>   
+        </div>
       </div>
-    </div>     
-    <div class="c-sessions__cta">
-      <p><a href="">Read our guidance</a></p>
     </div>
-  </div>      
+    <div class="c-sessions__cta">
+      <p><a class="c-button c-button--orange" href="">Read our guidance</a></p>
+    </div>
+
+    <hr />
+
+    <h2 id="in-person-sessions" class="c-sessions__header center-text">{{ $t('In-person sessions') }}</h2>
+    <p>In-person sessions can run as previously, provided they comply with any guidelines required by their venue and local health authorities. You may need to implement additional hygiene, social distancing and check-in measures.</p>
+    <div class="c-sessions__content">
+     <div class="c-sessions__content-individual">
+       <div class="c-sessions__text">
+         <p>The key considerations of running in-person sessions are: </p>
+         <ul>
+           <li>follow local public health advice for your region</li>
+           <li>talk with the venue and follow their requirements</li>
+           <li>Devise a suitable plan with our useful resource</li>
+           <li>Communicate your plan with all Dojo members</li>
+         </ul>
+       </div>
+     </div>
+     <div class="c-sessions__content-individual">
+       <div class="c-sessions__quote-cards">
+         <div class="c-sessions__quote-card">
+           <p class="c-sessions__quote">“{{ $t('Realising that I had created such a wonderful place for the children also ignited a spark in me.') }}”</p>
+           <div class="c-sessions__quote-attr">
+             <img class="c-sessions__quote-attr-image" src="~/static/images/startadojo/marloes.jpeg" />
+             <span class="c-sessions__quote-attr-text">Marloes, Netherlands</span>
+           </div>
+         </div>
+       </div>
+     </div>
+   </div>
+
+    <div class="c-sessions__content">
+      <div class="c-sessions__content-individual">
+        <div class="c-sessions__quote-cards">
+          <div class="c-sessions__quote-card">
+            <p class="c-sessions__quote">“{{ $t('Realising that I had created such a wonderful place for the children also ignited a spark in me.') }}”</p>
+            <div class="c-sessions__quote-attr">
+              <img class="c-sessions__quote-attr-image" src="~/static/images/startadojo/marloes.jpeg" />
+              <span class="c-sessions__quote-attr-text">Marloes, Netherlands</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="c-sessions__content-individual">
+        <div class="c-sessions__text">
+          <h3>Resources</h3>
+          <ul>
+            <li><a href="">In-person plannig checklist</a></li>
+            <li><a href="">Promotional characters and assets</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="c-sessions__cta">
+      <p><a class="c-button c-button--orange" href="">Read our guidance</a></p>
+    </div>
+    <h2>Adapting over time</h2>
+
+    <p>You may need to adapt and change how you run your club as restrictions in your local area change  and combine elements of online, remote and in-person activities, or you may do different things at different times. Here are some example1s from the community of how they have combined the above options.</p>
+    <hr />
+
+    <h2>Additional Support</h2>
+    <p>Join our <a href="https://rpf.io/ccalls">regular community calls</a>, connect with other community members on our <a href="https://coderdojo.com/2020/01/23/5-reasons-to-be-a-part-of-our-global-slack/">Global CoderDojo slack instance</a>, or let us know how you are getting on <a href="/contact/">by email</a>. </p>
+  </div>
 </template>
 
 <style lang="scss" scoped>
@@ -173,6 +181,10 @@
     max-width: $content-width;
     margin: 0 auto 40px;
     padding: 40px 0 40px;
+    &__in-person {
+
+    }
+
     &__content {
       display: flex;
       max-width: $content-width;
@@ -188,7 +200,7 @@
       p {
         margin-bottom: 40px;
       }
-    }    
+    }
     &__illustration {
       display: block;
       margin: 40px auto 0 auto;
@@ -221,7 +233,7 @@
           margin-left: 10px;
         }
       }
-    }   
+    }
     &__cta {
       text-align: center;
       margin: 80px auto 80px auto;
