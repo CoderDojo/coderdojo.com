c816ea2ed679cc54e2bb0d53f598c583b93a79af
Greg Annandale <greg@raspberrypi.org>
2020-11-02 15:48:14 +0000

    Adds maintenance page + component, incl. Twitter timeline embed

---
+++ components/maintenance/Hero.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="c-hero center-text">
+    <h1 class="c-hero__header1">Under maintenance</h1>
+    <div class="c-hero__intro">
+      <p>The CoderDojo website is undergoing maintenance. We'll be back as soon as we can!</p>
+      <p>For any urgent queries, please contact <a href="mailto:info@coderdojo.org">info@coderdojo.org</a></p>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "~/assets/_variables.scss";
+
+.c-hero {
+  padding: 0 0 70px 0;
+  background-color: $cd-purple;
+  &__header1 {
+    color: $white;
+    max-width: 900px;
+    margin: 0 auto;
+    padding: 50px 20px;
+    font-weight: bold;
+  }
+
+  &__intro,
+  &__outro {
+    max-width: $content-width;
+    margin: 0 auto;
+    color: $white;
+    text-align: center;
+
+    p:not(:last-child) {
+      padding-bottom: 1em;
+    }
+
+    a, a:link, a:visited {
+      color: #ffffff;
+    }
+  }
+
+  &__outro {
+    max-width: $content-width;
+    margin: 2em auto 0 auto;
+    color: $white;
+    text-align: center;
+
+    a {
+      color: $white;
+      text-decoration: underline;
+      font-weight: bold;
+    }
+  }
+}
+@include media-breakpoint-max(sm) {
+  .c-hero {
+    background-color: $cd-purple;
+    &__header1 {
+      font-size: $mobile-h1-size;
+      font-weight: $mobile-h1-weight;
+    }
+  }
+}
+</style>
---
+++ pages/maintenance.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <Hero />
+    <div class="c-twitter-embed">
+        <a class="twitter-timeline" href="https://twitter.com/CoderDojo?ref_src=twsrc%5Etfw">Tweets by CoderDojo</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+    </div>
+  </div>
+</template>
+
+<script>
+import Hero from "~/components/maintenance/Hero";
+export default {
+  components: {
+    Hero,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.c-twitter-embed {
+    max-width: 900px;
+    margin: 0 auto;
+    font-size: 1rem;
+}
+</style>
