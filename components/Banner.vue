e4c309945a1a888c33f2d4c30bc958cfe4b21662
Glen Pike <github@glenpike.co.uk>
2020-11-05 14:41:18 +0000

    Page is not translated

--- components/Banner.vue
+++ components/Banner.vue
@@ -5,7 +5,7 @@
         {{ $t('Thank you for your interest in CoderDojo!') }}
          {{ $t('The coronavirus pandemic continues to impact all our lives, including how Dojos can run.') }}
          {{ $t('See') }}
-        <a class="cd-banner-link" href="localePath({ name: 'ways-to-run' })">
+        <a class="cd-banner-link" href="/ways-to-run">
           {{ $t('our resources and guidance on the creative ways Dojos are running.') }}
         </a>
       </p>
